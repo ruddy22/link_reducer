18d41a57b29ea7ca818769d4b4b7066d1a1df6c0
Alexey Petrov <ruddy22@mail.ru>
2020-01-24 18:01:20 +0300

    Update server and table creation

--- index.js
+++ index.js
@@ -6,45 +6,71 @@
 
 const express = require('express');
 const _ = require('lodash');
+const shortId = require('shortid');
+const validUrl = require('valid-url');
 const knexOptions = require('./knexfile');
 const knex = require('knex')(knexOptions.development);
 
-// fns
-const idToShortLink = () => {
-
-};
-
-const shortLinkToId = () => {
-
-};
+const PORT = 5000;
+const ADDRESS = 'localhost';
+const PROTOCOL = 'http';
+const LINKS = 'links';
 
 // app
 const app = express();
 
 // starts server
-app.listen(5000, () => console.log('Server started! Checks http://localhost:5000'));
+app.listen(PORT, () => console.log(`Server started on port ${PORT}!`));
 
 // router
 app.get('/', async (req, res) => {
-  const result = await knex('links').select('*');
+  const result = await knex(LINKS).select('*');
 
   res.json({ message: 'Hello world', rowsInTable: result });
 });
 
-// app.post('/create_link', (req, res) => {
-app.get('/create_link', async (req, res) => {
-  const result = await knex('links')
-    .insert({ link: 'link', transition_count: 0 });
-  const id = _.first(result);
+const makeAnErrorObject = (errorText) => ({ status: 'error', errorText });
 
-  const shortLink = idToShortLink();
+const processUrl = async (url) => {
+  const generatedCode = shortId();
+  const shortUrl = `${PROTOCOL}://${ADDRESS}:${PORT}/${generatedCode}`;
 
-  res.json({ status: 'ok' });
+  try {
+    await knex(LINKS).insert({
+      original_link: url,
+      short_link: shortUrl,
+      generated_code: generatedCode,
+      transition_count: 0,
+    });
+  } catch (err) {
+    return makeAnErrorObject(err.message);
+  }
+
+  return {
+    status: 'ok',
+    data: {
+      original: url,
+      short: shortUrl
+    },
+  };
+};
+
+app.post('/create_link/:url', async (req, res) => {
+  const url = req.params.url || '';
+  const result = validUrl.isWebUri(url)
+    ? await processUrl(url)
+    : makeAnErrorObject('Invalid url.');
+
+  res.json(result);
 });
 
-app.get('/:shortId', async (req, res) => {
-  const linkId = shortLinkToId(shortId);
-  const result = await knex('links').first(['link']);
+app.get('/:code', async (req, res) => {
+  const linkCode = req.params.code;
+  const result = await knex(LINKS).first('original_link').where({ generated_code: linkCode });
 
-  res.redirect(result.link);
+  if (result) {
+    res.redirect(result.link);
+  } else {
+    res.json(makeAnErrorObject('Link not found.'));
+  }
 });
--- migrations/20200124143511_links.js
+++ migrations/20200124143511_links.js
@@ -3,8 +3,12 @@ const links = 'links';
 exports.up = (knex) => {
   return knex.schema.createTableIfNotExists(links, (table) => {
     table.increments();
-    table.string('link');
+    table.string('original_link');
+    table.string('short_link');
+    table.string('generated_code');
     table.integer('transition_count');
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   })
 };
 
