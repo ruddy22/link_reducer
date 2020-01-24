2b0b5e5625dcbd63eaf6c43dded8ae6a2a714ca5
Alexey Petrov <ruddy22@mail.ru>
2020-01-24 15:39:58 +0300

    Add server prototype

--- index.js
+++ index.js
@@ -5,8 +5,18 @@
  */
 
 const express = require('express');
+const _ = require('lodash');
 const knexOptions = require('./knexfile');
 const knex = require('knex')(knexOptions.development);
+
+// fns
+const idToShortLink = () => {
+
+};
+
+const shortLinkToId = () => {
+
+};
 
 // app
 const app = express();
@@ -19,4 +29,22 @@ app.get('/', async (req, res) => {
   const result = await knex('links').select('*');
 
   res.json({ message: 'Hello world', rowsInTable: result });
+});
+
+// app.post('/create_link', (req, res) => {
+app.get('/create_link', async (req, res) => {
+  const result = await knex('links')
+    .insert({ link: 'link', transition_count: 0 });
+  const id = _.first(result);
+
+  const shortLink = idToShortLink();
+
+  res.json({ status: 'ok' });
+});
+
+app.get('/:shortId', async (req, res) => {
+  const linkId = shortLinkToId(shortId);
+  const result = await knex('links').first(['link']);
+
+  res.redirect(result.link);
 });
